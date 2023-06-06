3f75141115ea6e02ea3990c7e2dd42b9b7643816
jayiterator <jayson.nm.paul@proton.me>
2023-06-06 13:09:24 +1000

    case statement

--- BashRefresher/myscript.sh
+++ BashRefresher/myscript.sh
@@ -2,7 +2,7 @@
 
 # ECHO COMMAND
 
-echo Hello World!
+# echo Hello World!
 
 #VARIABLES
 
@@ -38,13 +38,73 @@ NAME="Creator"
 
 # ELSE-IF (elif)
 
-if [ "$NAME" == "Jayson" ]
-then
-    echo "Your name is Jayson"
+# if [ "$NAME" == "Jayson" ]
+# then
+#     echo "Your name is Jayson"
 
-elif [ "$NAME" == "Tom" ]
-then
-    echo "Your name is Tom"
-else
-    echo "Your name is NOT Jayson or Tom"
-fi
+# elif [ "$NAME" == "Tom" ]
+# then
+#     echo "Your name is Tom"
+# else
+#     echo "Your name is NOT Jayson or Tom"
+# fi
+
+# COMPARISON
+
+# NUM1=3
+# NUM2=5
+# if [ "$NUM1" -gt "$NUM2" ]
+# then 
+#     echo "$NUM1 is greater than $NUM2"
+# else
+#     echo "$NUM1 is less than $NUM2"
+# fi
+
+########
+# -eq  : equal
+# -ne  : not equal
+# -gt  : greater than
+# -ge  : greater than or equal to
+# -lt  : less than
+# -le  : less than or equal to
+########
+
+# FILE CONDITIONS
+
+# FILE="test.txt"
+# if [ -e "$FILE" ]
+# then
+#     echo "$FILE exists"
+# else
+#     echo "$FILE does NOT exist"
+# fi
+
+##############
+
+# -d file   : True if the file is a directory
+# -e file   : True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   : True if the provided string is a file
+# -g file   : True if the group id is set on a file
+# -r file   : True if the file is readable
+# -s file   : True if the file has a non-zero size
+# -u        : True if the user id is set on a file
+# -w        : True if the file is writable
+# -x        : True if the file is an executable
+
+##############
+
+# CASE STATEMENT
+
+read -p "Are you 21 or over? Y/N " ANSWER
+
+case "$ANSWER" in
+    [yY] | [yY][eE][sS])
+        echo "You can have a beer. :)"
+    ;;
+    [nN] | [nN][oO])
+        echo "Sorry, no drinking."
+    ;;
+*)
+    echo "Please enter y/yes or n/no"
+    ;;
+esac
