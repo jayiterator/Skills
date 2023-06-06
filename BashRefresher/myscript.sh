ab546dc8b9bc5dcd3d3daba747e1a0dec810a3bc
jayiterator <jayson.nm.paul@proton.me>
2023-06-06 12:55:20 +1000

    Initial looping

--- BashRefresher/myscript.sh
+++ BashRefresher/myscript.sh
@@ -3,3 +3,48 @@
 # ECHO COMMAND
 
 echo Hello World!
+
+#VARIABLES
+
+# Uppercase by convention
+
+# Letters, numbers, underscores
+
+NAME="Creator"
+
+# echo "My name is ${NAME}."
+
+# USER INPUT
+
+# read -p "Enter your name: " NAME
+
+# echo "Hello ${NAME}, nice to meet you!"
+
+# SIMPLE IF STATEMENT
+
+# if [ "$NAME" == "Jayson" ]
+# then
+#     echo "Your name is Jayson"
+# fi
+
+# IF-ELSE
+
+# if [ "$NAME" == "Jayson" ]
+# then
+#     echo "Your name is Jayson"
+# else
+#     echo "Your name is NOT Jayson"
+# fi
+
+# ELSE-IF (elif)
+
+if [ "$NAME" == "Jayson" ]
+then
+    echo "Your name is Jayson"
+
+elif [ "$NAME" == "Tom" ]
+then
+    echo "Your name is Tom"
+else
+    echo "Your name is NOT Jayson or Tom"
+fi
